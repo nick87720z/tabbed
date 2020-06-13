79080a766acd749e12714a613c135946ce53019c
Nikita Zlobin <nick87720z@gmail.com>
2020-06-13 14:09:00 +0500

    Update client name on _NET_WM_TITLE change too
    I got problem with xterm, though not urxvt - when title randomly was not
    updated. By some reason sometimes WM_NAME prop event doesn't accompany
    one for _NET_WM_NAME, despite its changed, thus missing update. One
    nasty issue here - next update will supply previous name, though
    eventually gives actual name. Checking for both WM_NAME and _NET_WM_NAME
    does the fix.

--- tabbed.c
+++ tabbed.c
@@ -879,7 +879,7 @@ propertynotify(const XEvent *e)
 			}
 		}
 		XFree(wmh);
-	} else if (ev->state != PropertyDelete && ev->atom == XA_WM_NAME &&
+	} else if (ev->state != PropertyDelete && (ev->atom == XA_WM_NAME || ev->atom == wmatom[WMName]) &&
 	           (c = getclient(ev->window)) > -1) {
 		updatetitle(c);
 	}
